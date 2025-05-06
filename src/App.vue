16789c6919a1c54fed073648ee8cfb311f0f7173
lsm0428 <lsmlsm0428@gamil.com>
2025-05-06 20:32:13 +0900

    250506_vue_userlist1

--- src/App.vue
+++ src/App.vue
@@ -1,74 +1,77 @@
-<script setup lang="ts">
+<script setup>
+import Axios from "axios";
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+
+import { Container,
+  ListItem,
+  ListIcon,
+  ListHeader,
+  ListDescription,
+  ListContent,
+  List,
+  Pagination,
+  Search  } from "semantic-ui-vue";
+import { ref, onMounted } from 'vue'
+
+const getData = async () => {
+  const getUserList = [];
+    await Axios.get("http://localhost:8090/user/userList", {
+      headers :{
+                'Access-Control-Allow-Headers':'Content-Type, Authorization, Response-Header',
+                'Access-Control-Allow-Methods':'POST, GET, OPTIONS, DELETE',
+                'Access-Control-Allow-Origin':'*',
+                'Access-Control-Expose-Headers':'*'
+              },
+      params: {
+      },
+    }
+  ).then((response) => {
+      console.log("response.data : " + JSON.stringify(response.data));
+      getUserList.push(response.data);
+      console.log("getUserList : " + JSON.stringify(getUserList));
+      return "getUserList";
+
+    }).catch(function (error) {
+      return null;
+    });
+
+  }
+
+  await Axios.get("http://localhost:8090/user/userList", {
+      headers :{
+                'Access-Control-Allow-Headers':'Content-Type, Authorization, Response-Header',
+                'Access-Control-Allow-Methods':'POST, GET, OPTIONS, DELETE',
+                'Access-Control-Allow-Origin':'*',
+                'Access-Control-Expose-Headers':'*'
+              },
+      params: {
+      },
+    }
+  ).then((response) => {
+      console.log("response.data : " + JSON.stringify(response.data));
+      const userList = ref([response.data]);
+    }).catch(function (error) {
+    });
+
+
+  console.log("userList : " + userList.length);
+  userList.value.forEach((users, index) => {
+  // forEach의 콜백 함수 외부에 있는 `parentMessage`에 대한 접근 가능.
+  // 반면 `item`과 `index`는 콜백함수 내부에서만 접근 가능.
+  console.log("users : " + users);
+})
+
+  onMounted(() => {
+
+})
+
+
 </script>
 
 <template>
 <div>
-
+  <li v-for="({ users }, index) in userList">
+  {{ users }} {{ index }}
+</li>
 </div>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
