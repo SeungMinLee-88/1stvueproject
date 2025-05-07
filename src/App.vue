830f67c74c695806c7edd6d45fbc727dfb15e9da
lsm0428 <lsmlsm0428@gamil.com>
2025-05-07 18:23:53 +0900

    vue_prj2

--- src/App.vue
+++ src/App.vue
@@ -2,76 +2,119 @@
 import Axios from "axios";
 import { RouterLink, RouterView } from 'vue-router'
 
-import { Container,
-  ListItem,
-  ListIcon,
-  ListHeader,
-  ListDescription,
-  ListContent,
-  List,
-  Pagination,
-  Search  } from "semantic-ui-vue";
 import { ref, onMounted } from 'vue'
 
-const getData = async () => {
-  const getUserList = [];
-    await Axios.get("http://localhost:8090/user/userList", {
-      headers :{
-                'Access-Control-Allow-Headers':'Content-Type, Authorization, Response-Header',
-                'Access-Control-Allow-Methods':'POST, GET, OPTIONS, DELETE',
-                'Access-Control-Allow-Origin':'*',
-                'Access-Control-Expose-Headers':'*'
-              },
-      params: {
-      },
-    }
-  ).then((response) => {
-      console.log("response.data : " + JSON.stringify(response.data));
-      getUserList.push(response.data);
-      console.log("getUserList : " + JSON.stringify(getUserList));
-      return "getUserList";
-
-    }).catch(function (error) {
-      return null;
-    });
-
-  }
-
-  await Axios.get("http://localhost:8090/user/userList", {
-      headers :{
-                'Access-Control-Allow-Headers':'Content-Type, Authorization, Response-Header',
-                'Access-Control-Allow-Methods':'POST, GET, OPTIONS, DELETE',
-                'Access-Control-Allow-Origin':'*',
-                'Access-Control-Expose-Headers':'*'
-              },
-      params: {
-      },
-    }
-  ).then((response) => {
-      console.log("response.data : " + JSON.stringify(response.data));
-      const userList = ref([response.data]);
-    }).catch(function (error) {
-    });
 
 
-  console.log("userList : " + userList.length);
-  userList.value.forEach((users, index) => {
+const userListref = ref([]);
+console.log("userListref aaaaaaaaaa : " + JSON.stringify(userListref));
+
+//      Axios.get("http://localhost:8090/user/userList", {
+// /*       headers :{
+//                 'Access-Control-Allow-Headers':'Content-Type, Authorization, Response-Header',
+//                 'Access-Control-Allow-Methods':'POST, GET, OPTIONS, DELETE',
+//                 'Access-Control-Allow-Origin':'*',
+//                 'Access-Control-Expose-Headers':'*'
+//               }, */
+//       params: {
+//       },
+//     }
+//   ).then((response) => {
+//       console.log("response.data : " + JSON.stringify(response.data));
+//       userListref.value = response.data;
+//       console.log("userListref 11111111 : " + JSON.stringify(userListref));
+
+
+//     }).catch(function (error) {
+//       console.log("error : " + error)
+//     });
+//     console.log("userListref 2222222222 : " + JSON.stringify(userListref));
+//     //console.log("userListref loginId : " + JSON.stringify(userListref.value[0].loginId));
+//     userListref.value.forEach((users, index) => {
+//   console.log("users : " + users[index].loginId);
+// })
+
+const getData = () => {
+
+Axios.get("http://localhost:8090/user/userList", {
+/*       headers :{
+            'Access-Control-Allow-Headers':'Content-Type, Authorization, Response-Header',
+            'Access-Control-Allow-Methods':'POST, GET, OPTIONS, DELETE',
+            'Access-Control-Allow-Origin':'*',
+            'Access-Control-Expose-Headers':'*'
+          }, */
+  params: {
+  },
+}
+).then((response) => {
+  console.log("response.data : " + JSON.stringify(response.data));
+  userListref.value = response.data;
+  console.log("userListref 11111111 : " + JSON.stringify(userListref));
+
+
+}).catch(function (error) {
+  console.log("error : " + error)
+});
+console.log("userListref 2222222222 : " + JSON.stringify(userListref));
+//console.log("userListref loginId : " + JSON.stringify(userListref.value[0].loginId));
+userListref.value.forEach((users, index) => {
+console.log("users : " + users[index].loginId);
+})
+}
+
+
+
+
+  console.log("userListref 000000000 : " + JSON.stringify(userListref));
+/*   userListref.value.forEach((users, index) => {
+  console.log("users : " + JSON.stringify(users))
+}) */
+
   // forEach의 콜백 함수 외부에 있는 `parentMessage`에 대한 접근 가능.
   // 반면 `item`과 `index`는 콜백함수 내부에서만 접근 가능.
-  console.log("users : " + users);
+  const items = ref([{ message: 'Foo' }, { message: 'Bar' }]);
+  console.log("items 11111111 : " + JSON.stringify(items));
+
+  userListref.value.forEach((users, index) => {
+  console.log("users : " + users[index].loginId);
 })
 
-  onMounted(() => {
 
+onMounted(() => {
+  getData();
+  console.log("call onMounted");
 })
-
-
 </script>
-
 <template>
 <div>
-  <li v-for="({ users }, index) in userList">
-  {{ users }} {{ index }}
-</li>
+  <li v-for="(userList, index) in userListref">
+  - index : {{ index }} - userList : {{ userList }}
+  </li>
 </div>
+<sui_list divided relaxed>
+    <sui_list_item>
+      <sui_list_icon name="github" size="large" vertical_align="middle" />
+      <sui_list_content>
+        <a is="sui_list_header">Semantic_Org/Semantic_UI</a>
+        <a is="sui_list_description">Updated 10 mins ago</a>
+      </sui_list_content>
+    </sui_list_item>
+    <sui_list_item>
+      <sui_list_icon name="github" size="large" vertical_align="middle" />
+      <sui_list_content>
+        <a is="sui_list_header">Semantic_Org/Semantic_UI_Docs</a>
+        <a is="sui_list_description">Updated 22 mins ago</a>
+      </sui_list_content>
+    </sui_list_item>
+    <sui_list_item>
+      <sui_list_icon name="github" size="large" vertical_align="middle" />
+      <sui_list_content>
+        <a is="sui_list_header">Semantic_Org/Semantic_UI_Meteor</a>
+        <a is="sui_list_description">Updated 34 mins ago</a>
+      </sui_list_content>
+    </sui_list_item>
+  </sui_list>
+
+
+
 </template>
--- src/main.js
+++ src/main.js
@@ -5,10 +5,15 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import SemanticUI from 'semantic-ui-vue';
+
+import 'semantic-ui-css/semantic.min.css';
+
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(SemanticUI);
 
 app.mount('#app')
